5c7a388ac36301f2bf780cdf4c2830084cf42a15
JoeWard1977 <jward@lfanet.org>
2022-11-15 18:45:13 +0000

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,33 @@
+import 'package:brew_crew/models/customuser.dart';
+import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // Replace with actual values
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDM8tp76jdKgupSNPLr-Rvrv1VNZoqkE2k",
+      appId: "XXX",
+      messagingSenderId: "XXX",
+      projectId: "brew-crew-3a6cf",
+    ),
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return StreamProvider<CustomUser?>.value(
+        value: AuthService().user,
+        initialData: null,
+        child: MaterialApp(home: Wrapper()));
   }
 }
